540e7e78b20ca30b3a4785fdc29bb7ba19fb1600
Kadu Cury <carlosercury@gmail.com>
2023-11-16 23:48:44 -0300

    Add Titulo Nova Anotacao Modal

--- anotacoesScript.js
+++ anotacoesScript.js
@@ -9,3 +9,32 @@ document.getElementById('buttonVoltar').addEventListener('click', function() {
 document.getElementById('buttonSair').addEventListener('click', function() {
     window.location.href = 'index.html';
 });
+
+// Modal Título do Novo Doc Engine
+var modalCadastro = document.getElementById('setarTituloNovoDoc'); 
+var openModalCadastro = document.getElementById('anotacaoPlus'); 
+var buttonCloseCadastro = document.getElementById('fecharTituloNovoDoc');
+
+openModalCadastro.onclick = function() {
+    modalCadastro.showModal();
+}
+
+buttonCloseCadastro.onclick = function() {
+    modalCadastro.close();
+};
+
+document.getElementById('buttonCadastrar').addEventListener('click', function() {
+    modalCadastro.classList.add('show'); // Adiciona a classe show ao abrir
+});
+
+buttonCloseCadastro.onclick = function() {
+    modalCadastro.classList.remove('show'); // Remove a classe show ao fechar
+};
+
+document.getElementById('fecharCadastro').addEventListener('click', function() {
+    modalCadastro.close();
+});
+
+document.getElementById('buttonCadastrar').addEventListener('click', function() {
+    modalCadastro.showModal();
+});
